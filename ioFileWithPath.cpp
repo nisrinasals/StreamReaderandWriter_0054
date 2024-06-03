996844ab22b11063ed72801119b25e74d26e8c34
nisrinasals <nisrinahikmawan@gmail.com>
2024-06-03 15:16:31 +0700

    membuat nama file dalam int main

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -3,3 +3,18 @@
 #include <string>
 using namespace std;
 
+int main(){
+    string baris;
+    string NamaFile;
+
+    cout << "Masukkan Nama File : ";
+    cin >> NamaFile;
+
+    //membuka file dalam mode menulis
+    ofstream outfile;
+    //menunjuk ke sebuah nama file
+    outfile.open(NamaFile + ".txt", ios::out);
+
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+}
