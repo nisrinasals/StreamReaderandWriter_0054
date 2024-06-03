a6a45feb0a3dfb0b6b27d0508d5b04139d8a7e44
nisrinasals <nisrinahikmawan@gmail.com>
2024-06-03 15:10:52 +0700

    memnuat loop jika file yang akan dimunculkan ada

--- ioFile.cpp
+++ ioFile.cpp
@@ -31,4 +31,11 @@ int main(){
     //menunjuk ke sebuah file
     infile.open("Contohfile.txt");
 
+    cout << endl << ">= Membuka dan membaca file" << endl;
+    //jika file ada maka
+    if (infile.is_open())
+    {
+        //melakukan perulangan setiap baris
+        while (getline(infile, baris))
+
 }
