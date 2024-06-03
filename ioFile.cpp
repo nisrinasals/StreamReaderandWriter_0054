1e8d64e3f4edfc516e865913dd160db15a4eb484
nisrinasals <nisrinahikmawan@gmail.com>
2024-06-03 15:09:26 +0700

    menutup file yang selesai ditulis

--- ioFile.cpp
+++ ioFile.cpp
@@ -23,5 +23,7 @@ int main(){
         //menulis dan memasukkan nilai dari 'baris' ke dalam file
         outfile << baris << endl;
     }
+    //selesai dalam menuliskan tutup filenya
+    outfile.close();
 
 }
