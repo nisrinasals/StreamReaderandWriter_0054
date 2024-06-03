cb657392b8545b5e1f809edc1719728a6ea4c50b
nisrinasals <nisrinahikmawan@gmail.com>
2024-06-03 15:12:10 +0700

    menampilkan pemberitahuan jikafile tidak bisa di buka

--- ioFile.cpp
+++ ioFile.cpp
@@ -44,5 +44,7 @@ int main(){
         //tutup file tersebut setelah selesai
         infile.close();
     }
-
+    //jika tidak ditemukan maka file akan menampilkan ini
+    else cout << "Unable to open file";
+    return 0;
 }
