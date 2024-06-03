ed82135a884be144d3e8cf57c7f814ae803dcf24
nisrinasals <nisrinahikmawan@gmail.com>
2024-06-03 15:11:37 +0700

    menampilkan dan menutup file setelah selesai

--- ioFile.cpp
+++ ioFile.cpp
@@ -37,5 +37,12 @@ int main(){
     {
         //melakukan perulangan setiap baris
         while (getline(infile, baris))
+        {
+            //dan tampilkan di sini
+            cout << baris << '\n';
+        }
+        //tutup file tersebut setelah selesai
+        infile.close();
+    }
 
 }
